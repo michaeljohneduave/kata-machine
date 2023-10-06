fba02966180e068334338e9c01e90998b7dfe92a
Michael Eduave <michaeljohneduave@gmail.com>
2023-10-05 18:46:47 -0700

    Trees!

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    while (q.length) {
+        const next = q.shift()!;
+
+        if (next.left) {
+            q.push(next.left);
+        }
+
+        if (next.right) {
+            q.push(next.right);
+        }
+
+        if (next.value === needle) {
+            return true;
+        }
+    }
+
+    return false;
 }
--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,25 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function visit(node: BinaryNode<number> | null, needle: number): boolean {
+    if (!node) {
+        return false;
+    }
 
+    if (node.value === needle) {
+        return true;
+    }
+
+    // right side
+    if (node.value < needle) {
+        return visit(node.right, needle);
+    }
+
+    // left side
+    if (node.value >= needle) {
+        return visit(node.left, needle);
+    }
+
+    return false;
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return visit(head, needle);
 }
